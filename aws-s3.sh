4bff71eff2b0339eaa58282e4c6bf948c35c9e79
Ubuntu <ubuntu@ip-10-0-0-12.ec2.internal>
2023-07-19 18:13:34 +0000

    edit aws script

--- aws-s3.sh
+++ aws-s3.sh
@@ -13,4 +13,4 @@ mysqldump -u root -h $DB_HOST -p$DB_PASSWORD $DB_NAME > /tmp/$BACKUP && \
 export AWS_ACCESS_KEY_ID=AKIAJ6DYDN73DAFICOGQ && \
 export AWS_SECRET_ACCESS_KEY=$AWS_SECRET && \
 echo "Uploading your $BACKUP backup" && \
-awsv2 s3 cp /tmp/db-$DATE.sql s3://$BUCKET_NAME/$BACKUP
+awsv2 s3 cp /tmp/$BACKUP s3://$BUCKET_NAME/$BACKUP
